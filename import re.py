b4f51ed6bd9ef49d4335d190a6f1bdc18723f7f9
Ale Rosso <103438237+Iare7@users.noreply.github.com>
2022-07-15 15:04:05 -0300

    Add files via upload
    Aca te dejo el archivito brodi

---
+++ import re.py
@@ -0,0 +1,13 @@
+print("Hola patienzo")
+
+print("Aca vamos a meter unos 3 numeritos como para probar")
+
+n1=int(input("Aca vamos a meter un numerito re groso"))
+n2=int(input("Aca vamos a meter otro numerito re groso"))
+n3=int(input("Aca vamos a meter otro numerito re groso"))
+
+suma=n1+n2+n3
+prom=suma/3
+
+print("el promedio de los 3 numeritos es: ",prom)
+
