221b0f4d1fd5b73ed76c4e584d2fd5328f15fc92
Ale Rosso <103438237+Iare7@users.noreply.github.com>
2022-07-18 19:13:48 -0300

    Update import re.py

--- import re.py
+++ import re.py
@@ -2,9 +2,9 @@ print("Hola patienzo")
 
 print("Aca vamos a meter unos 3 numeritos como para probar")
 
-n1=int(input("Aca vamos a meter un numerito re groso"))
-n2=int(input("Aca vamos a meter otro numerito re groso"))
-n3=int(input("Aca vamos a meter otro numerito re groso"))
+n1=int(input("Aca vamos a meter un numerito re groso "))
+n2=int(input("Aca vamos a meter otro numerito re groso "))
+n3=int(input("Aca vamos a meter otro numerito re groso "))
 
 suma=n1+n2+n3
 prom=suma/3
